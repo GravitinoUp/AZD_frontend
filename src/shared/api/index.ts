b75a52a0bd504b534563aa2e94c5df749da3d71b
ION <thegoshagames@gmail.com>
2024-06-26 16:40:04 +0300

    fix env var

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { getCookieValue } from '../lib/get-cookie-value'
 
 export const apiRequest = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL,
+    baseURL: import.meta.env.VITE_API,
     headers: {
         Authorization: `Bearer ${getCookieValue('accessToken')}`,
     },
