d89804afd168e53752a3a635a373dee0d6aac1f0
ION <thegoshagames@gmail.com>
2024-06-26 17:23:39 +0300

    .

--- src/pages/error-page/error-page.tsx
+++ src/pages/error-page/error-page.tsx
@@ -1,7 +1,13 @@
-export const ErrorPage = () => (
-    <div className="flex-center h-screen flex-col gap-5">
-        <h1 className="text-5xl font-bold">Упс!</h1>
-        <p className="text-3xl">Произошла непредвиденная ошибка.</p>
-        <p className="text-2xl">Ничего не найдено</p>
-    </div>
-)
+import { useTranslation } from 'react-i18next'
+
+export const ErrorPage = () => {
+    const { t } = useTranslation()
+
+    return (
+        <div className="flex-center h-screen flex-col gap-5">
+            <h1 className="text-5xl font-bold">Упс!</h1>
+            <p className="text-3xl">{t('error.default')}</p>
+            <p className="text-2xl">{t('nothing.found')}</p>
+        </div>
+    )
+}
